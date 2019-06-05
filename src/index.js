f6d473e64195b4198c54245a5126dd49c3f43e89
Pridestalkerr <xcruzherblade@gmail.com>
2019-06-05 21:49:00 +0300

    oop, indentation test

--- src/index.js
+++ src/index.js
@@ -3,90 +3,134 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
-class Grid extends React.Component{
-	constructor(props)
-	{
+class Tetris extends React.Component {
+	constructor(props) {
 		super(props);
 
-		this.state = {x: 5, y: 0, on: 1};
-		
-		this.coordinates = [];
-		
-		this.table = new Array(11);
-		for(let itr = 0; itr < 11; ++itr)
-			this.table[itr] = new Array(11).fill(false);
-		
-		this.newBlock = this.newBlock.bind(this);
-		this.move = this.move.bind(this);
+		this.state = {
+			x: Math.floor(this.props.boardSize / 2),
+			y: 0,
+			on: true
+		};
+
+		this.table = new Array(this.props.boardSize);
+		for (let itr = 0; itr < this.props.boardSize; ++itr)
+			this.table[itr] = new Array(this.props.boardSize).fill(false);
+
+		this.checkMove = this.checkMove.bind(this);
+		this.moveLeft = this.moveLeft.bind(this);
+		this.moveRight = this.moveRight.bind(this);
 		this.fall = this.fall.bind(this);
 	}
-	newBlock(props)
-	{
-		this.table[props.y][props.x] = true;
-		this.coordinates.push({x: props.x, y: props.y});
-	}
-	move(event)
-	{
-		var audio = new Audio(process.env.PUBLIC_URL + "/pop.mp3");
-		audio.play();
 
-		if(event.code === "ArrowDown")
-		{
-			this.fall();
-			return;
-		}
-
-		let x = this.state.x;
-		if(event.code === "ArrowLeft")
-			--x;
-		else if(event.code === "ArrowRight")
-			++x;
-		if(!(x >= 11 || x < 0 || this.table[this.state.y][x] === true))
-			this.setState({x: x});
-	}
-
-	fall()
-	{
-		let y = this.state.y + 1;
-		if(y >= 11 || this.table[y][this.state.x] === true)
-		{
-			var audio = new Audio(process.env.PUBLIC_URL + "/pop.mp3");
-			audio.play();
-			if(this.state.y === 0)
-			{
-				console.log("GAME OVER");
-				this.setState({on: 0});
-				window.removeEventListener("keydown", this.move);
-				clearInterval(this.fallInterval);
-			}
-			this.newBlock({x: this.state.x, y: this.state.y});
-			this.setState({x: 5, y: 0});
-		}
-		else
-			this.setState({y: y});
-	}
-
-	componentWillMount()
-	{
-		window.addEventListener("keydown", this.move);
+	componentWillMount() {
+		window.addEventListener("keydown", this.checkMove);
 		this.fallInterval = setInterval(this.fall, 500);
 	}
 
-	componentWillUnmount()
-	{
-		window.removeEventListener("keydown", this.move);
+	componentWillUnmount() {
+		window.removeEventListener("keydown", this.checkMove);
 		clearInterval(this.fallInterval);
 	}
 
-	render()
+	checkMove(event) {
+		if (event.code === "ArrowDown") 
+			this.fall();
+		else if (event.code === "ArrowLeft") 
+			this.moveLeft();
+		else if (event.code === "ArrowRight") 
+			this.moveRight();
+	}
+
+	moveLeft() {
+	new Audio(process.env.PUBLIC_URL + "/pop.mp3").play();
+	if (!(this.table[this.state.y][this.state.x - 1] || this.state.x === 0))
+	this.setState({ x: this.state.x - 1 });
+	}
+
+	moveRight() {
+		new Audio(process.env.PUBLIC_URL + "/pop.mp3").play();
+		if (!(this.table[this.state.y][this.state.x + 1] || this.state.x === this.props.boardSize - 1))
+			this.setState({ x: this.state.x + 1 });
+	}
+
+	fall() {
+		let y = this.state.y + 1;
+		if (y >= this.props.boardSize || this.table[y][this.state.x]) 
+		{
+			new Audio(process.env.PUBLIC_URL + "/pop.mp3").play();
+			if (this.state.y === 0) 
+			{
+				this.setState({ on: false });
+				window.removeEventListener("keydown", this.checkMove);
+				clearInterval(this.fallInterval);
+			}
+			this.table[this.state.y][this.state.x] = true;
+			this.setState({ x: Math.floor(this.props.boardSize / 2), y: 0 });
+		} 
+		else 
+			this.setState({ y: y });
+	}
+
+	render() 
 	{
 		return (
-			<div style = {{width: 550, height: 550, backgroundColor: "#000000"}}>
-				{this.coordinates.map(({ x, y }, index) => <div key = {index} style = {{position: "absolute", width: 50, height: 50, backgroundColor: "#00FF00", marginLeft: x * 50, marginTop: y * 50}}></div>)}
-				{this.state.on === 1 && <div style = {{position: "absolute", width: 50, height: 50, backgroundColor: "#00FF00", marginLeft: this.state.x * 50, marginTop: this.state.y * 50}}></div>}
-				{this.state.on === 0 && <h1 style = {{position: "absolute", color: "#FF0000"}} >GAME OVER</h1>}
-			</div> );
+			<div
+				style = {{
+					width: this.props.boardSize * this.props.blockSize,
+					height: this.props.boardSize * this.props.blockSize,
+					backgroundColor: "#000000",
+					outline: "2px solid #00FF00"
+				}}
+			>
+			{
+				this.table.map((line, i) =>
+					line.map((ok, j) => {
+						if (ok)
+							return (
+								<div
+									key = {i.toString() + j.toString()}
+									style = {{
+										position: "absolute",
+										width: this.props.blockSize,
+										height: this.props.blockSize,
+										backgroundColor: "#00FF00",
+										marginLeft: j * this.props.blockSize,
+										marginTop: i * this.props.blockSize
+									}}
+								/>
+							);
+					})
+				)
+			}
+			{
+				this.state.on ? 
+				(
+					<div
+						style = {{
+							position: "absolute",
+							width: this.props.blockSize,
+							height: this.props.blockSize,
+							backgroundColor: "#00FF00",
+							marginLeft: this.state.x * this.props.blockSize,
+							marginTop: this.state.y * this.props.blockSize
+						}}
+					/>
+				) : (
+					<h1
+						style = {{ 
+							position: "absolute", 
+							color: "#FF0000", 
+							margin: "auto" 
+						}}
+					>
+						GAME OVER
+					</h1>
+				)
+			}
+			</div>
+		);
 	}
 }
 
-ReactDOM.render(<Grid/>, document.getElementById("board"));
+ReactDOM.render(<Tetris boardSize = {11} blockSize = {50} />, document.getElementById("board"));

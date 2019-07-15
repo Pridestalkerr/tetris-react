808b41df1373762b09a00eb4263069dfa39296f1
Pridestalkerr <xcruzherblade@gmail.com>
2019-07-15 18:16:49 +0300

    Updates

---
+++ precache-manifest.1be6a8cbf7e7652df9decb133416b6ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e8ee457cce8f3f8868548d145599fd",
+    "url": "/tetris-react/index.html"
+  },
+  {
+    "revision": "cb7c8022e8c2bbad9c11",
+    "url": "/tetris-react/static/css/main.7afbeda8.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/tetris-react/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "cb7c8022e8c2bbad9c11",
+    "url": "/tetris-react/static/js/main.e0babac6.chunk.js"
+  },
+  {
+    "revision": "5ffb780af0f8d98f40e3",
+    "url": "/tetris-react/static/js/runtime~main.009b4986.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.955abc700cbbce626081faf73260f3db.js"
+  "/tetris-react/precache-manifest.1be6a8cbf7e7652df9decb133416b6ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tetris-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

26202fd86f87b9e455faacba28c23ecb2d858316
Pridestalkerr <xcruzherblade@gmail.com>
2019-07-16 12:55:02 +0300

    added a tutorial in page

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 import Tetris from "./components/tetris.jsx";
 import "./index.css";
 
-ReactDOM.render(<Tetris boardSize = {20} blockSize = {25}/>, document.getElementById("main"));
+ReactDOM.render(<Tetris boardSize = {20} blockSize = {25}/>, document.getElementById("game"));
